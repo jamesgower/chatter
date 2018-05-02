dabf014c98bcd68d179bf7530128bbccd00fdf9e
James <james_gower@outlook.com>
2018-05-02 09:19:40 +0100

    greet new user and alert others on new connection

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', socket => {
 	console.log('New user connected.');
 
+	socket.emit('newMessage', {
+		from: 'Admin',
+		text: 'Welcome to the chatroom!',
+	});
+
+	socket.broadcast.emit('newMessage', {
+		from: 'Admin',
+		text: 'A new user has joined the chat!',
+	});
+
 	socket.on('createMessage', message => {
 		console.log('New message:', message);
 		io.emit('newMessage', {
