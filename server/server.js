2ee4b260e9da197be02d2d04837dbd8af8805dcc
James <james_gower@outlook.com>
2018-05-02 12:15:23 +0100

    added functionality to send geolocation on click of a button and added tests to support this

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
-const { generateMessage } = require('./utils/message');
+const { generateMessage, generateLocationMessage } = require('./utils/message');
 
 app.use(express.static(publicPath));
 
@@ -23,6 +23,10 @@ io.on('connection', socket => {
 		console.log('New message:', message);
 		io.emit('newMessage', generateMessage(message.sender, message.text));
 		callback('This is from the server');
+	});
+
+	socket.on('createLocationMessage', coords => {
+		io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
 	});
 
 	socket.on('disconnect', socket => {
--- server/utils/message.js
+++ server/utils/message.js
@@ -1,9 +1,18 @@
-var generateMessage = (sender, text) => {
+const generateMessage = (sender, text) => {
 	return {
 		sender,
 		text,
-		createdAt: new Date().getTime()
-	}
-}
+		createdAt: new Date().getTime(),
+	};
+};
 
-module.exports = { generateMessage };
+const generateLocationMessage = (sender, latitude, longitude) => {
+	const url = `https://www.google.co.uk/maps?q=${latitude},${longitude}`;
+	return {
+		sender,
+		url,
+		createdAt: new Date().getTime(),
+	};
+};
+
+module.exports = { generateMessage, generateLocationMessage };
