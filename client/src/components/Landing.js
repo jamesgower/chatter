0d390d6c8d90e28368ddb8e4767dc248c9acf7c9
James <james_gower@outlook.com>
2018-08-30 11:53:21 +0100

    updated scrollToBottom function to make it work for location messages, added unique identifiers for display names which are the same as others, added readme.md

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
     InputGroup,
     InputGroupButtonDropdown,
@@ -8,67 +8,84 @@ import {
     DropdownToggle,
     DropdownMenu,
     DropdownItem,
-    Tooltip
-} from 'reactstrap';
-import Chat from './Chat';
-import {isRealString} from '../server/utils/validation';
+    Tooltip,
+} from "reactstrap";
+import Chat from "./Chat";
+import { isRealString } from "../server/utils/validation";
 
 class Landing extends React.Component {
     constructor() {
         super();
 
         this.state = {
-            name: '',
-            room: '',
+            name: "",
+            room: "",
             nameError: false,
             roomError: false,
             dropdownOpen: false,
-            submitted: false
+            submitted: false,
         };
-        this.toggleDropDown = this
-            .toggleDropDown
-            .bind(this);
+        this.toggleDropDown = this.toggleDropDown.bind(this);
     }
 
-    componentWillMount() {
-        document.title = 'Join | Chatter'
-    }
-
+    /*
+        Open dropdown for room change on firing
+    */
     toggleDropDown() {
         this.setState({
-            dropdownOpen: !this.state.dropdownOpen
+            dropdownOpen: !this.state.dropdownOpen,
         });
-    };
+    }
 
     onSubmit(e) {
         e.preventDefault();
+        /*
+            Check if name & room are valid strings. If they are, then set relevant state 
+            to match the strings, and set submitted state to true.
+        */
         if (isRealString(this.state.name) && isRealString(this.state.room)) {
-            this.setState({submitted: true});
+            this.setState({ submitted: true });
         } else {
+            /*
+                If either of the strings are not valid, then set the relevant error state
+                to true, and show the relevant error.
+            */
             if (!isRealString(this.state.name)) {
-                this.setState({nameError: true});
+                this.setState({ nameError: true });
                 setTimeout(() => {
-                    this.setState({nameError: false});
+                    this.setState({ nameError: false });
                 }, 2500);
             }
             if (!isRealString(this.state.room)) {
-                this.setState({roomError: true});
+                this.setState({ roomError: true });
                 setTimeout(() => {
-                    this.setState({roomError: false});
+                    this.setState({ roomError: false });
                 }, 2500);
             }
         }
     }
 
+    /*
+        Change the room state to the value which has been typed/clicked on in the dropdown menu
+        or room text field.
+    */
     onRoomChange(e) {
-        this.setState({room: e.target.value});
+        this.setState({ room: e.target.value });
     }
 
+    /*
+        Change the name state to the value which has been typed in the name text field.
+    */
     onNameChange(e) {
-        this.setState({name: e.target.value});
+        this.setState({ name: e.target.value });
     }
 
-    render() {    
+    render() {
+        document.title = "Join | Chatter";
+        /*
+            If the user has not tried to connect to a room, the landing page will be shown t
+            allow the user to choose a room.
+        */
         if (!this.state.submitted) {
             return (
                 <div className="centered-form">
@@ -78,31 +95,74 @@ class Landing extends React.Component {
                         </div>
                         <div className="form-field">
                             <Label>Display name</Label>
-                            <Input type="text" id="nameInput" autoFocus onChange={e => this.onNameChange(e)}/>
+                            <Input
+                                type="text"
+                                id="nameInput"
+                                autoFocus
+                                onChange={e => this.onNameChange(e)}
+                            />
                         </div>
                         <div className="form-field">
                             <Label>Room name</Label>
                             <InputGroup id="roomInput">
+                                {/* 
+                                    When the input is changed, it will update the room state with
+                                    the value which the user has input via the onRoomChange function.
+                                */}
                                 <Input
                                     type="text"
                                     value={this.state.room}
-                                    onChange={(e) => this.onRoomChange(e)}/>
+                                    onChange={e => this.onRoomChange(e)}
+                                />
                                 <InputGroupButtonDropdown
                                     addonType="append"
                                     isOpen={this.state.dropdownOpen}
-                                    toggle={this.toggleDropDown}>
-                                    <DropdownToggle caret>
-                                        Select
-                                    </DropdownToggle>
+                                    toggle={this.toggleDropDown}
+                                >
+                                    <DropdownToggle caret>Select</DropdownToggle>
                                     <DropdownMenu>
+                                        {/* 
+                                            onRoomChange() sets the current room state to be the same as 
+                                            the chosen dropdown option
+                                        */}
                                         <DropdownItem header>Rooms</DropdownItem>
-                                        <DropdownItem value="React & Redux" onClick={(e) => this.onRoomChange(e)}>React & Redux</DropdownItem>
-                                        <DropdownItem value="JavaScript" onClick={(e) => this.onRoomChange(e)}>JavaScript</DropdownItem>
-                                        <DropdownItem value="New Frameworks" onClick={(e) => this.onRoomChange(e)}>New Frameworks</DropdownItem>
-                                        <DropdownItem value="Discuss" onClick={(e) => this.onRoomChange(e)}>Discuss</DropdownItem>
-                                        <DropdownItem value="Design Ideas" onClick={(e) => this.onRoomChange(e)}>Design Ideas</DropdownItem>
-                                        <DropdownItem divider/>
-                                        <DropdownItem value="Chill Zone" onClick={(e) => this.onRoomChange(e)}>Chill Zone</DropdownItem>
+                                        <DropdownItem
+                                            value="React & Redux"
+                                            onClick={e => this.onRoomChange(e)}
+                                        >
+                                            React & Redux
+                                        </DropdownItem>
+                                        <DropdownItem
+                                            value="ES6+ JavaScript"
+                                            onClick={e => this.onRoomChange(e)}
+                                        >
+                                            ES6+ JavaScript
+                                        </DropdownItem>
+                                        <DropdownItem
+                                            value="New Frameworks"
+                                            onClick={e => this.onRoomChange(e)}
+                                        >
+                                            New Frameworks
+                                        </DropdownItem>
+                                        <DropdownItem
+                                            value="Discuss"
+                                            onClick={e => this.onRoomChange(e)}
+                                        >
+                                            Discuss
+                                        </DropdownItem>
+                                        <DropdownItem
+                                            value="Design Ideas"
+                                            onClick={e => this.onRoomChange(e)}
+                                        >
+                                            Design Ideas
+                                        </DropdownItem>
+                                        <DropdownItem divider />
+                                        <DropdownItem
+                                            value="Chill Zone"
+                                            onClick={e => this.onRoomChange(e)}
+                                        >
+                                            Chill Zone
+                                        </DropdownItem>
                                     </DropdownMenu>
                                 </InputGroupButtonDropdown>
                             </InputGroup>
@@ -111,30 +171,28 @@ class Landing extends React.Component {
                             <Button color="success">Join {this.state.room}</Button>
                         </div>
                     </form>
-                    <Tooltip
-                        placement="top"
-                        isOpen={this.state.nameError}
-                        target="nameInput"
-                    >
+                    {/* Below are the error tooltips which fire when there is an error in the form input */}
+                    <Tooltip placement="top" isOpen={this.state.nameError} target="nameInput">
                         Please insert a valid display name.
                     </Tooltip>
-                    <Tooltip
-                        placement="bottom"
-                        isOpen={this.state.roomError}
-                        target="roomInput"
-                    >
+                    <Tooltip placement="bottom" isOpen={this.state.roomError} target="roomInput">
                         Please insert a valid room name
                     </Tooltip>
                 </div>
-            )
+            );
         } else {
+            /*
+                If the user has submitted a room to enter and a username, then the Chat component
+                will be rendered, with the name and room as props passed to it.
+            */
             return (
                 <div>
                     <Chat
                         params={{
-                        name: this.state.name,
-                        room: this.state.room
-                    }}/>
+                            name: this.state.name,
+                            room: this.state.room,
+                        }}
+                    />
                 </div>
             );
         }
--- client/src/server/server.js
+++ client/src/server/server.js
@@ -1,79 +1,128 @@
-const express = require('express');
-const http = require('http');
-const path = require('path');
-const socketIO = require('socket.io');
-const publicPath = path.join(__dirname, '../../build');
-const port = process.env.PORT || 5000;
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const socketIO = require("socket.io");
+const publicPath = path.join(__dirname, "../../build");
 const app = express();
 const server = http.createServer(app);
-const {generateMessage, generateLocationMessage} = require('./utils/message');
-const {isRealString} = require('./utils/validation');
-const {Users} = require('./utils/users');
+const { generateMessage, generateLocationMessage } = require("./utils/message");
+const { isRealString } = require("./utils/validation");
+const { Users } = require("./utils/users");
 const io = socketIO(server);
 const users = new Users();
 
 app.use(express.static(publicPath));
 
-io.on('connection', socket => {
-    console.log('New user connected.');
+/*
+    The io.on connection function is the manager which controls all connections and messages in
+    the whole application. It will log a statement when a new user is connected to any room.
+*/
+io.on("connection", socket => {
+    console.log("New user connected.");
 
-    socket.on('join', (params, callback) => {
+    socket.on("join", (params, callback) => {
+        /* 
+            The parameters are checked to be real strings, and uses the callback function
+            if either are not valid, to say that they are required to connect to a room.
+        */
         if (!isRealString(params.name) || !isRealString(params.room)) {
-            return callback('Name and room name are required.');
+            return callback("Name and room name are required.");
         }
-        socket.join(params.room); //joins the room which was specified in index.html params
+
+        /*
+            If there is more than one user in the chosen chat room with the same name as the one
+            which the original user has picked, then the name will be appended with squared brackets
+            and a number to make each user unique. (e.g Bill[1]).
+        */
+        if(users.getUserList(params.room).indexOf(params.name) > -1) {
+            const usersArr = users.getUserList(params.room);
+            const matches = usersArr.filter(user => params.user !== user).length;
+            params.name = `${params.name}[${matches + 1}]`
+        };
+        /*
+            If there are no errors, the user will be connected to the room which is specified
+            in the params.room value.
+        */
+        socket.join(params.room);
+        /*
+            The user is removed from all other rooms when added to the new room
+        */
         users.removeUser(socket.id);
-        users.addUser(socket.id, params.name, params.room)
-        io
+        /*
+            The users' values are added to the chosen rooms' users array, and the updateUserList function is
+            emitted to update the sidebar with all of the users in the rooms values. The newMessageAdmin function 
+            is then emitted to alert all users apart from the current user in the room that the new user has entered 
+            the room, alongside an admin message that only the current user will receive which welcomes them to the room. 
+        */
+        users.addUser(socket.id, params.name, params.room);
+        io.to(params.room).emit("updateUserList", users.getUserList(params.room));
+        socket.emit(
+            "newMessageAdmin",
+            generateMessage("Admin", `Welcome to the ${params.room} room.`),
+        );
+        socket.broadcast
             .to(params.room)
-            .emit('updateUserList', users.getUserList(params.room));
-        // socket.leave(params.room); //will leave the targeted room
-        // io.to(params.room).emit //sends an event to everybody in the targeted room
-        // socket.broadcast.to(params.room).emit //sends a message to everybody in the
-        // targeted room apart from the one that sent it
-        socket.emit('newMessageAdmin', generateMessage('Admin', `Welcome to the ${params.room} room.`));
-        socket
-            .broadcast
-            .to(params.room)
-            .emit('newMessageAdmin', generateMessage('Admin', `${params.name} has joined the chat room.`));
+            .emit(
+                "newMessageAdmin",
+                generateMessage("Admin", `${params.name} has joined the chat room.`),
+            );
         callback();
     });
 
-    socket.on('createMessage', (message, callback) => {
-        const user = users.getUser(socket.id)
-
+    /*
+        The socket.on createMessage function emits an event which both sends a message which shows as a blue message, sent
+        from the user to all others in the room; and a green message, which shows up to other users as a received message.
+    */
+    socket.on("createMessage", (message, callback) => {
+        const user = users.getUser(socket.id);
         if (user && isRealString(message.text)) {
-            socket
-                .broadcast
-                .emit('newMessage', generateMessage(user.name, message.text));
-            socket.emit('newMessageSent', generateMessage(user.name, message.text));
+            socket.broadcast.emit("newMessage", generateMessage(user.name, message.text));
+            socket.emit("newMessageSent", generateMessage(user.name, message.text));
         }
         callback();
     });
 
-    socket.on('createLocationMessage', coords => {
+    /*
+        The socket.on createLocationMessage function emits an event which both sends a message with a link containing the users current location,
+        which shows as a blue message, sent from the user to all others in the room; and a green location message, which shows up to other users 
+        as a received message.
+    */
+    socket.on("createLocationMessage", coords => {
         const user = users.getUser(socket.id);
         if (user) {
-            socket
-                .broadcast
-                .emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude)); //send as a recieved message to all users in room but sender
-            socket.emit('newLocationMessageSent', generateLocationMessage(user.name, coords.latitude, coords.longitude)); // looks as if user has sent the message (different colour and side)
+            socket.broadcast.emit(
+                "newLocationMessage",
+                generateLocationMessage(user.name, coords.latitude, coords.longitude),
+            );
+            socket.emit(
+                "newLocationMessageSent",
+                generateLocationMessage(user.name, coords.latitude, coords.longitude),
+            );
         }
     });
 
-    socket.on('disconnect', () => {
+    /*
+        The socket.on disconnect function updates the UserList sidebar inside the current chat room
+        when a user leaves. It also emits a red message, which looks like it comes from the administrator,
+        saying that the user has left the room.
+    */
+    socket.on("disconnect", () => {
         const user = users.removeUser(socket.id);
         if (user) {
-            io
-                .to(user.room)
-                .emit('updateUserList', users.getUserList(user.room));
-            io
-                .to(user.room)
-                .emit('newMessage', generateMessage('Admin', `${user.name} has left the room.`));
+            io.to(user.room).emit("updateUserList", users.getUserList(user.room));
+            io.to(user.room).emit(
+                "newMessage",
+                generateMessage("Admin", `${user.name} has left the room.`),
+            );
         }
     });
 });
 
+/*
+    The port is set to be the PORT environment variable which is set by Heroku, if the node environment is not in development,
+    or if it is, then it will use port 5000 - which can be accessed from localhost:5000.
+*/
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
     console.log(`Server is up at port ${port}`);
 });
